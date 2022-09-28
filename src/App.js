8c0f91d41260fe4b2b90e267accd8a8cc4bdbfc6
roabaz <robazter@gmail.com>
2022-09-28 12:48:48 -0300

    desafio contador

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import "./App.css";
 import { NavBar } from "./includes/navBar";
 import { SearchBar } from "./includes/searchBar";
 import { Main } from "./includes/main";
-
+import { useState } from "react";
 
 function App() {
+
+
   return (
     <div className="container p-5">
-      
-          <SearchBar/>
-          <hr/>
-          <NavBar/>
-          <hr/>
-          <Main content="Hello World, this is cubiC Ecommerce"/>
-      </div>
+      <SearchBar />
+      <hr />
+      <NavBar />
+      <hr />
+      <Main content="Hello World, this is cubiC Ecommerce" />
+    </div>
   );
 }
 
--- src/includes/main.jsx
+++ src/includes/main.jsx
@@ -1,11 +1,33 @@
 
+import React, {useState} from 'react';
+
 export const Main = (params) => {
+    const [count, setCountValue] = useState(0);
+
+    const addCount = () => {
+      setCountValue(count + 1);
+    };
+  
+    const lessCount = () => {
+      if (count > 0) {
+        setCountValue(count - 1);
+      }
+    };
+    const clearCount = () => {
+        if (count > 0) {
+          setCountValue(count - count);
+        }
+      };
     return (
 
         <div className="container text-center p-5">
 
-        {params.content}
+            <p>{params.content}</p>
 
+            <button className="btn btn-primary mx-2"> {count} </button>
+            <button className="btn btn-success mx-2" onClick={addCount}>+</button>
+            <button className="btn btn-warning mx-2" onClick={lessCount}>-</button>
+            <button className="btn btn-danger mx-2" onClick={clearCount}>0</button>
         </div>
 
     );
